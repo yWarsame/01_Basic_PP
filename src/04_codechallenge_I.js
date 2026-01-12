2b9aa83afb3f00fad4a936ee1c2e3e944c8986bb
Yasin Warsame <yasinhassan1013@gmail.com>
2026-01-12 14:08:34 +0000

    eine lösung Variant

--- src/04_codechallenge_I.js
+++ src/04_codechallenge_I.js
@@ -10,8 +10,11 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 */
 
 const prompt = require('prompt-sync')({sigint: true});
-const num1 = +prompt("tippen sie die erst Zahl ein: "); // + konvertiert zu Number
-const num2 = +prompt("nun tippen sie die zweite Zahl ein: ");
+
+const num1 = Number(prompt("Tippen Sie die erste Zahl ein: "));
+const num2 = Number(prompt("Nun tippen Sie die zweite Zahl ein: "));
 const sum = num1 + num2;
 
+console.log("Datentype num1: " + typeof num1);
+console.log("Datentype num2: " + typeof num2);
 console.log(`Die Summe der Zahlen ist: ${sum}`);
