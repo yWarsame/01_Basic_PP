508534a53b91769cdf637c58e415ed1559ff3b47
jy4education-gif <jy4education@gmail.com>
2026-01-12 10:59:43 +0000

    Aktualisiere package.json, um die Hauptdatei auf 03_prompt.js zu setzen und füge Abhängigkeit für prompt-sync hinzu; erstelle .gitignore für node_modules und füge package-lock.json hinzu.

--- src/03_prompt.js
+++ src/03_prompt.js
@@ -1,8 +1,12 @@
-// --> installiern mit: npm i prompt-sync
+// --> installiern mit: npm i neuerPrompt-sync
 
-const prompt = require('prompt-sync')({sigint: true});
+const neuerPrompt = require('prompt-sync')({sigint: true});
 
 // Alles muss man selber machen lassen! ...
-let firstName = prompt("Vorname?: ");
-let familyName = prompt("Name?: ");
+let firstName = neuerPrompt("Vorname?: ");
+let familyName = neuerPrompt("Name?: ");
 console.log("Hallo, " + firstName + " " + familyName +"!");
+console.log("Ein Markt belohnt nicht Fleiß, sondern Positionierung!");
+
+// npm i prompt -sync
+// npm init -y
