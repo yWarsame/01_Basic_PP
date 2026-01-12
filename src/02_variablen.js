5d369b86d44cb81556bb571827e1c0c860f175cb
jy4education-gif <jy4education@gmail.com>
2026-01-12 08:09:22 +0000

    Aktualisiere Hauptdatei in package.json und kommentiere Code in 02_variablen.js

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -9,14 +9,26 @@
  */
 
 /***** Variablen 01 *******/
-
-
+// let myVariable = 1;
+// var anotherVariable = "HI!";
+// const myConstant = 42;
+// console.log(myVariable, anotherVariable, myConstant);   
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * /
 
 /* Deklaration + Wertzuweisung I */
 
+// let hi = "Hello Class!";
+// console.log(hi);
+// console.log('Hello, World!');
 
-
-
+// var ho = "Ciao Class!";
+// console.log(ho);
 
 
 
@@ -25,11 +37,42 @@
     JS ist ein untypisierte Sprache!
 */
 
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
+
+
+// let firstName; // Deklaration (Definition)
+// console.log(firstName); // Ausgabe
+// firstName = "Max"; // Wertzuweisung | assignment
+// console.log(firstName); // Ausgabe
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+// console.log("Hi, ich bin " + firstName + " " + familyName + "!"); // Ausgabe
+// console.log(`Hi, ich bin ${firstName} ${familyName}!`); // Ausgabe mit Template-String (ES6)
+
+// firstName = 2;
+// console.log("Mein Vorname ist jetzt:", firstName);
+// firstName = true;
+// console.log("Mein Vorname ist jetzt:", firstName);
 
 
 
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(function() {
+//         console.log("var i ist:", i);
+//     }, 100);
+// }
 
-
+// for (let i = 0; i < 3; i++) {
+//     setTimeout(function() {
+//         console.log("let i ist:", i);
+//     }, 100);
+// }
 
 /* Variablen vs. Konstanten in JS */
 
+
+//  Wir können 
+//  ganz 
+//  viele Kommentare
+//  hinterlegen
