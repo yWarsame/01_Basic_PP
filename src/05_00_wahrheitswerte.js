75051bd51a69498a72574500c55ded2d95c01884
jy4education-gif <jy4education@gmail.com>
2026-01-13 10:15:10 +0000

    Updated 05_00

--- src/05_00_wahrheitswerte.js
+++ src/05_00_wahrheitswerte.js
@@ -3,11 +3,26 @@
 
 // Deklaration
 let ageJohn, ageMark;
-const birthYearJohn = 1980;
+const birthYearJohn = 1990;
 const birthYearMark = 1990;
 
-// Berechnung Alter
-let year = 2024;
+// Berechnung Alter - Iteration I
+// let year = 2026;
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+
+// Berechnung Alter - Iteration II
+// let date = new Date();
+// console.log("Datum: " + date);
+// let year = date.getFullYear();
+// console.log("Jahr: " + year);
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// Berechnung Alter - Iteration III
+let year = new Date().getFullYear();
+console.log("Jahr: " + year);
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
@@ -15,3 +30,9 @@ ageMark = year - birthYearMark;
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 
+// Deklaration | Tests | Aussagenlogik
+// let isJohnOlder = false;
+let isJohnOlder = (ageJohn > ageMark); // Test auf Ungleichheit
+let isJohnEqual = (ageJohn == ageMark) // Test auf Gleichheit
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
