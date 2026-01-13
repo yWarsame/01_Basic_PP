c9fb52faa4253ac8c5931e96f3c292a8e300267c
Yasin Warsame <yasinhassan1013@gmail.com>
2026-01-13 09:26:57 +0000

    Wahrheitswert commited

---
+++ src/05_wahrheitswerte.js
@@ -0,0 +1,17 @@
+
+/* Berechnung Alter */
+
+// Deklaration
+let ageJohn, ageMark;
+const birthYearJohn = 1980;
+const birthYearMark = 1990;
+
+// Berechnung Alter
+let year = 2024;
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+
