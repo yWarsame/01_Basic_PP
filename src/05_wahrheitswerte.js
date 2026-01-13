49a0e3439e82b85b0fcae1442a294fefc268eca6
Yasin Warsame <yasinhassan1013@gmail.com>
2026-01-13 09:51:18 +0000

    Iteration I-III

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -6,10 +6,32 @@ let ageJohn, ageMark;
 const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
-// Berechnung Alter
-let year = 2024;
+// Berechnung Alter - Iteration I
+/**let year = 2024;
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
+*/
+
+/* Berechnung Alter - Iteration II
+let date = new Date();
+console.log("Datum: " +date);
+let year = date.getFullYear();
+console.log("Jahr:"+year);
+ageJohn = year-birthYearJohn;
+ageMark = year-birthYearMark;
+*/
+// Berechnung Alter - Iteration III
+let year = new Date().getFullYear();
+console.log("Jahr:"+year);
+ageJohn = year-birthYearJohn;
+ageMark = year-birthYearMark;
+
+
+
+/*ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
+*/
+
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
