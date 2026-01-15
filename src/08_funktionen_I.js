fdc35a30d61d12937ac5c606fc78e67e22ab593f
Yasin Warsame <yasinhassan1013@gmail.com>
2026-01-15 16:11:37 +0000

    08 / 03b Verantwortlichkeiten in getrennte Funktionen auslagern

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -2,45 +2,27 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
-// Funktionaufruf | Call
-//test ();
+// Funktionsaufruf | call
+// test();
 
-//Funktionrumpf | callee
-//Funktiondeklaration
-
-/*function test ()
-
+// Funktionsrumpf | (callee)
+// Funktionsdeklaration
+function test()
 {
-
-    console.log ("Hallo , Simonas! ");
-
+    console.log("Hallo, Simonas!");
 }
-*/
 
 /***** Funktionen 02a *****/
 // 2a. Parametrisierung + Datenübergabe von INNEN
-    // call
 
-/*
-    
-    ausgabeName();
-    // Funktionalität
-function ausgabeName() 
-{
-    // interne Variable | What happens in VEGAS stays in VEGAS :)
-    let firstname="Simonas";
+// call
+// ausgabeNamen();
 
-    console.log("Hallo ," + firstname +"! ")
-    
-}
-
-// console.log(firstname);  //Fehler  What happens in VEGAS stays in VEGAS :)
-
-*/
-
+// Funktionalität
 function ausgabeNamen() {
+    // interne Variable | What happens in VEGAS ...
     let firstName = "Yasin";
-    console.log("Hallo, " + firstName + "!");
+    console.log("Hallo, " + firstName  + "!");
 }
 
 // console.log(firstName); // Fehler: Scope
@@ -48,86 +30,67 @@ function ausgabeNamen() {
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-//ausgabeNamen2();
-//ausgabeNamen2("Yasin"); // Argument --> Daten für Parameter
-//ausgabeNamen2("Tarik");
-//ausgabeNamen2("Sandra");
-//
-//function ausgabeNamen2(firstName) { // Parameter
-  //  console.log("Hallo, " + firstName + "!");
-//};
+// ausgabeNamen2();
+// ausgabeNamen2("Yasin"); // Argument --> Daten für Parameter
+// ausgabeNamen2("Tarik");
+// ausgabeNamen2("Sandra");
 
-// function ausgabeNamen3(firstName, familyName) {  // Parameter
-//     console.log("Hallo, " + firstName + " " + familyName + "!");
-// }
-//ausgabeNamen3();
-/*ausgabeNamen3("Max", "Mütze");
-
-function ausgabeNamen3(firstName, familyName) {
-    console.log("Hallo, " + firstName + " " + familyName + "!");
+function ausgabeNamen2(firstName) { // Parameter
+    console.log("Hallo, " + firstName  + "!");
 }
-*/
+
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter / Argumente
 
-//ausgabeNamen3();
-
-//ausgabeNamen3("Max", "Mütze");
+// ausgabeNamen3();
+// ausgabeNamen3("Max", "Mütze");
 
 // Argumente werden von prompt() geliefert --> Input
-//const prompt = require('prompt-sync')({sigint: true});
-//ausgabeNamen3(prompt("Vorname: "),prompt("Name: "));
-/*
-function ausgabeNamen3(firstName, familyName) { // Parameter
+// const prompt = require('prompt-sync')({sigint: true});
+// ausgabeNamen3(prompt("Vorname: "),prompt("Name: "));
+
+function ausgabeNamen3(firstName, familyName) {  // Parameter
     console.log("Hallo, " + firstName + " " + familyName + "!");
 }
-*/
+
 /***** Funktionen 03a *****/
 // 03a. Vorbereitung -Trennen der Verantwortlichkeiten
-// Postulat: one fnction = one job (uncle Bob)
+// Postulat: one function = one job (uncle Bob)
 // SRP single responsibility principle
-//ausgabeNamenSRP("Max", "Mütze");
-/*
-function ausgabeNamenSRP(firstName, familyName) { // Parameter
-     //1st responsibility: string composing
-     const GAP = " ";
-     let outputStr = "Hallo, " + firstName + " " + familyName + "!" ;
+
+// ausgabeNamenSRP("Max", "Mütze");
+
+function ausgabeNamenSRP(firstName, familyName) {  // Parameter
+
+    //1st responsibility: string composing
+    const GAP = " ";
+    let outputStr = "Hallo, " + firstName + GAP + familyName + "!"
+
     //2nd responsibility: string output
     console.log(outputStr);
+
 }
-*/
 
 /***** Funktionen 03b *****/
 // 03b Verantwortlichkeiten in getrennte Funktionen auslagern
+
+
 //1st responsibility: string composing
-/*output(getString("Max, Mütze"));
+ output(getString("Max","Mütze"));
+
+
 function getString(firstName, familyName ) {
-
     const GAP = " ";
-    let outputStr = "Hallo, " + firstName + GAP + familyName + "!";
-    return outputStr;  //FKT.- OUTPUT sendet Daten zum call
-
+    let outputStr = "Hallo, " + firstName + GAP + familyName + "!"
+    output("test");
+    return outputStr; // Fkt.- OUTPUT sendet Daten zum call
+    output("hi");  // Abbruch NACH return
 }
 
 // 2nd responsibility: string output
 // output("hi");
 // output(2);
 // output(true);
-function output(outputData) {
+ function output(outputData) {
     console.log(outputData);
-
-}
-
-*/
-output(getString("Max", "Mütze"));  
-function getString(firstName, familyName) {
-    const GAP = " ";
-    let outputStr = "Hallo, " + firstName + GAP + familyName + "!";
-    output ("test !");
-    return outputStr; // Fkt.- OUTPUT sendet Daten zum call.
-    output ("hi"); // Abbruch nach return
-}
-
-function output(outputData) {
-    console.log(outputData);
-}
+ }
