147ff581530ccd6262aaff9a3c4919e0aa83fc2e
Yasin Warsame <yasinhassan1013@gmail.com>
2026-01-15 10:12:18 +0000

    2c. Mehrere Parameter / Argumente

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -21,7 +21,7 @@
 // 2a. Parametrisierung + Datenübergabe von INNEN
     // call
 
-
+/*
     
     ausgabeName();
     // Funktionalität
@@ -36,3 +36,48 @@ function ausgabeName()
 
 // console.log(firstname);  //Fehler  What happens in VEGAS stays in VEGAS :)
 
+*/
+
+function ausgabeNamen() {
+    let firstName = "Yasin";
+    console.log("Hallo, " + firstName + "!");
+}
+
+// console.log(firstName); // Fehler: Scope
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+//ausgabeNamen2();
+//ausgabeNamen2("Yasin"); // Argument --> Daten für Parameter
+//ausgabeNamen2("Tarik");
+//ausgabeNamen2("Sandra");
+//
+//function ausgabeNamen2(firstName) { // Parameter
+  //  console.log("Hallo, " + firstName + "!");
+//};
+
+// function ausgabeNamen3(firstName, familyName) {  // Parameter
+//     console.log("Hallo, " + firstName + " " + familyName + "!");
+// }
+//ausgabeNamen3();
+/*ausgabeNamen3("Max", "Mütze");
+
+function ausgabeNamen3(firstName, familyName) {
+    console.log("Hallo, " + firstName + " " + familyName + "!");
+}
+*/
+/***** Funktionen 02c *****/
+// 2c. Mehrere Parameter / Argumente
+
+//ausgabeNamen3();
+
+ausgabeNamen3("Max", "Mütze");
+
+// Argumente werden von prompt() geliefert --> Input
+const prompt = require('prompt-sync')({sigint: true});
+ausgabeNamen3(prompt("Vorname: "),prompt("Name: "));
+
+function ausgabeNamen3(firstName, familyName) { // Parameter
+    console.log("Hallo, " + firstName + " " + familyName + "!");
+}
