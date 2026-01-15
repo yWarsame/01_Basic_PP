268121fcde7e053067c9cb970bd677c044eb7516
Yasin Warsame <yasinhassan1013@gmail.com>
2026-01-15 09:22:39 +0000

    let firstname;  // Außerhalb

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -3,15 +3,36 @@
 // 1. Kapselung von Codeblöcken
 
 // Funktionaufruf | Call
-test ();
+//test ();
 
 //Funktionrumpf | callee
 //Funktiondeklaration
 
-function test ()
+/*function test ()
 
 {
 
     console.log ("Hallo , Simonas! ");
 
 }
+*/
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+    // call
+
+
+    
+    ausgabeName();
+    // Funktionalität
+function ausgabeName() 
+{
+    // interne Variable | What happens in VEGAS stays in VEGAS :)
+    let firstname="Simonas";
+
+    console.log("Hallo ," + firstname +"! ")
+    
+}
+
+// console.log(firstname);  //Fehler  What happens in VEGAS stays in VEGAS :)
+
