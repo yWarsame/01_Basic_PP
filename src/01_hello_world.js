f9b9a1af0bc501869deb7b5b7e4964fb45a54773
jy4education_git <jy4education@gmail.com>
2026-01-15 18:51:01 +0100

    +

--- src/01_hello_world.js
+++ src/01_hello_world.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
  console.log("Hello, World!");
  console.log(new Date().toLocaleString());
  console.log(`Hello, World it is ${new Date().toLocaleString()}`);
  console.log(new Date().toISOString());
 // Test
  console.log("Hello, World", new Date().toLocaleTimeString());
-=======
-console.log("Hello World");
-let msg = "this is a string";
-var msg2 = "this is another string";
->>>>>>> 6631710 (Add string variables to hello world script)
