a491b9b4e624e81ea085915aa17e826bce5b0aab
Yasin Warsame <yasinhassan1013@gmail.com>
2026-01-16 11:36:01 +0000

    10-Calculator

--- src/10_funktionen_II.js
+++ src/10_funktionen_II.js
@@ -8,8 +8,8 @@
 3. Fkt. Grundrechenarten : 
 4. Ausgabe in Konsole : check!
 */
-
-
+const Error_STR_DIV = "Teilen durch 0 nicht möglich!";
+const Error_STR_GEN ="Fehler: Unbekannter Operator!";
 // module: multiplication a * b |  test:
 // output(multiply(3,2));
 // output(multiply(3,-2));
@@ -38,13 +38,13 @@ function add(a,b) {
 /** WIE sollen die Aufgaben gelöst werden? */
 
 // module: division a / b |  test:
-output(divide(4,2));
+/*output(divide(4,2));
 output(divide(3,2));
 output(divide(3,-2));
 output(divide(0,2));
 output(divide(3,0));
 output(divide(0,0));
-
+*/
 /*function divide(a, b) {
   if (b == 0) {
     return "Teilen durch 0 nicht möglich";
@@ -52,6 +52,12 @@ output(divide(0,0));
   return a / b;
 }*/
 
+function divide(a, b) {
+  if (b == 0) {
+    return Error_STR_DIV;
+  } else 
+  return a / b;
+}
 
 // module: output | test:
 // output("hello");
@@ -59,3 +65,34 @@ output(divide(0,0));
 function output(outputData) {
 	console.log(outputData);
 }
+
+function calculator(a, b, op) {
+  switch (op) {
+    case "+": // Addition
+      return add(a, b);
+
+    case "-": // Subtraktion
+      return subtract(a, b);
+
+    case "*": // Multiplikation
+      return multiply(a, b);
+
+    case ":": // Division
+    case "/":
+      return divide(a, b);
+
+    default:
+      return Error_STR_GEN;
+  }
+}
+
+// Testaufrufe
+/*
+output(calculator(3, 2, "+"));
+output(calculator(3, 2, "-"));
+output(calculator(3, 2, "*"));
+output(calculator(3, 2, ":"));
+output(calculator(3, 2, "/"));
+output(calculator(3, 0, "/"));
+output(calculator(3, 2, "#?!"));*/
+
