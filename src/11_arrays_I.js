4a635656d20b5227860f3f082e26d51725ab267d
Yasin Warsame <yasinhassan1013@gmail.com>
2026-01-19 10:58:41 +0000

    11 / 01a. Funktionalität mit Array 1

---
+++ src/11_arrays_I.js
@@ -0,0 +1,75 @@
+
+/* Die Satzbau-Maschine | Arrays */
+
+/**
+ *   THEORIE
+ * /
+
+/*** 02. Theorie: Array 
+let arr;
+//arr=new Array();
+
+arr = [2,11,7,8,10,20]; // Arr. mit Elementen
+console.log(arr);
+console.log(arr.length); // Anzahl der Elemente
+console.log(arr[0]); // Index 0 (1.Pos. im Array)
+console.log(arr[5]); // // Letzt Pos. Elementen.
+console.log( arr[arr.length - 1]); // Letzt Pos. Elementen.
+
+*/
+
+
+ /*   PRAXIS
+ * 
+ */
+
+
+/*** 01. Funktionalität mit Einzelparametern */
+// --> "Ich bin Max Mütze."
+//output(getSentence("Ich","bin","Max","Mütze"));
+//output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+/*
+output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4,word5,word6) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   word1 + GAP +
+                word2 + GAP +
+                word3 + GAP +
+                word4 + GAP +
+                word5 + GAP +
+                word6 +
+                PUNCT;
+
+    return str;
+}
+*/
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation: semantisches Problem --> numerisches Problem
+//output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+    return str;
+}
+
+
+
+// Modul: Ausgabe in Konsole : Test
+//output("hi"); 
+function output(outputStr) {
+    console.log(outputStr);
+}
+
+
