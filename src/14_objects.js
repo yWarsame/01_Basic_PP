70407e96c563d6151b3f8903af3715d389d1fd91
Yasin Warsame <yasinhassan1013@gmail.com>
2026-01-20 10:05:54 +0000

    14 / Objekte I

--- src/14_objects.js
+++ src/14_objects.js
@@ -1,4 +1,4 @@
-// /***** Erinnerung: Arrays *******/
+/***** Erinnerung: Arrays *******/
 
 // let arr;
 // arr = ["Ich","bin","Max"];
@@ -29,32 +29,43 @@
 
 
 /***** Objekte 1 Daten/Funktionen *******/
-let person = {
-    firstName: "Yasin",
-    lastName: "Warsame",
-    salary: [120000, 160000],
-    permission: true, 
-    sayHello:function(){ return "Hello"}
 
+let person = {
+                firstName:"Yasin",
+                lastName: "Warsame",
+                salary: [120000, 160000],
+                permission: true,
+                sayHello: function(){return "Hallo"},
+                sayHello2:function () {
+                   return "Hallo, ich bin " + this.firstName;
+                }
 };
 
-output(person);
-output("---");
+// output(person);
+output("---------------");
 output(person.firstName);
 output(person.lastName);
-output(person["firstName"]);
-output("---");
+output(person["lastName"])
+output("---------------");
 output(person.salary[1]);
 for (let i = 0; i < person.salary.length; i++) {
     output(person.salary[i]);
 }
-output("---");
+output("---------------");
 output(person.permission);
+
+output("---------------")
+const txt = person.sayHello() + ", ich bin " + person.firstName +
+            " und verdiene " + person.salary[1] + " p.a.";
+output(txt);
+output("---------------")
+
+const txt2 = person.sayHello2() + " und verdiene " + person.salary[1] + " p.a.";
+output(txt2);
+
 
 // Modul: Ausgabe | Test
 //output("hi");
-
 function output(outputData) {
-    // Hier kommt die Implementierung der output-Funktion
     console.log(outputData);
 }
